0b4bfa064ab815520d576751d9aea6190c17ad25
diopibrahim <163294833+diopibrahim@users.noreply.github.com>
2024-03-31 10:16:25 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
+
+
+Matieres =[
+  {
+    "id": 1,
+    "title" : "Noraniya",
+    "description" :"Noraniya is based in good ways to read the coran ",
+      },
+
+  {
+    "id": 2,
+    "title" : "Arabe",
+    "description" :"Arabe langage by alphabet and vocabulary  ",
+      },
+  {
+    "id": 3,
+    "title" : "Tarbiya",
+    "description" :"Tarbiya islamiya means how pratice our religion  ",
+      },
+]
 
 app= Flask(__name__)
 @app.route("/")
 def home():
- return "Hello hjj "
-
+ return render_template("home.html",matieres=Matieres)
+  
+@app.route("/api/matieres")
+def list_matieres():
+  return jsonify(Matieres)
 
 app.run(host='0.0.0.0',debug=True)
 
